cbf9c67727e4497a9aabcf6676197d76d11b7e50
Alessio Manieri <mani.alexander03@gmail.com>
2019-11-17 17:39:01 +0100

    .

--- MethodsLibrary/BasicMaths.cs
+++ MethodsLibrary/BasicMaths.cs
@@ -6,7 +6,7 @@ namespace MethodsLibrary
     {
         public static double Add(double num1,double num2)
         {
-            return num1 + num2 ;
+            return num1 + num2;
         }
         public static double Substract(double num1, double num2)
         {
